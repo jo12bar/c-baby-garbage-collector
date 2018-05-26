a93d6d37d2374f9b8f5bfbbe324fbfbfdc5d61e1
jo12bar <johann.b@telus.net>
2018-05-25 22:48:09 -0700

    Implement a simple VM

--- main.c
+++ main.c
@@ -30,3 +30,50 @@ typedef struct sObject {
 		};
 	};
 } Object;
+
+/**
+ * Now, we implement a "virtual machine" of sorts. Really its only role is to
+ * have a stack to store the variables that are not in scope. Therefore, it's
+ * good for storing local variables and temporary variables needed in the
+ * middle of an expression.
+ */
+
+/** Maximum size of the VM stack. */
+#define STACK_MAX 256
+
+/** A simple virtual machine for managing out-of-scope variables. */
+typedef struct {
+	Object* stack[STACK_MAX];
+	int stackSize;
+} VM;
+
+/**
+ * Creates and initializes a new VM.
+ * @return A pointer to the new VM
+ */
+VM* newVM() {
+	VM* vm = malloc(sizeof(VM));
+	vm->stackSize = 0;
+	return vm;
+}
+
+/**
+ * Push an Object to a VM's stack.
+ * @param {VM*} vm Pointer to the VM
+ * @param {Object*} obj Pointer to the Object to be pushed
+ */
+void push(VM* vm, Object* obj) {
+	assert(vm->stackSize < STACK_MAX, "Stack overflow!");
+	vm->stack[vm->stackSize++] = obj;
+}
+
+/**
+ * Pop an Object from the VM's stack.
+ * @param {VM*} vm Pointer to the VM
+ * @return Pointer to the popped Object
+ */
+Object* pop(VM* vm) {
+	assert(vm->stackSize > 0, "Stack underflow!");
+	return vm->stack[--vm->stackSize];
+}
+
