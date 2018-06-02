376e39880c9a78564bd8fb5b1410a75b612a69c5
jo12bar <johann.b@telus.net>
2018-06-01 21:56:41 -0700

    Add tests.

--- main.c
+++ main.c
@@ -216,6 +216,101 @@ void objectPrint(Object* obj) {
 	}
 }
 
+/*******|
+| TESTS |
+|*******/
+
+void test1() {
+	printf("Test 1: Objects on stack are preserved.\n");
+	VM* vm = newVM();
+	pushInt(vm, 1);
+	pushInt(vm, 2);
+
+	gc(vm);
+	assert(vm->numObjects == 2, "Should have preserved objects.");
+	freeVM(vm);
+}
+
+void test2() {
+	printf("Test 2: Unreached objects are collected.\n");
+	VM* vm = newVM();
+	pushInt(vm, 1);
+	pushInt(vm, 2);
+	pop(vm);
+	pop(vm);
+
+	gc(vm);
+	assert(vm->numObjects == 0, "Should have collected objects.");
+	freeVM(vm);
+}
+
+void test3() {
+	printf("Test 3: Reach nested objects.\n");
+	VM* vm = newVM();
+
+	pushInt(vm, 1);
+	pushInt(vm, 2);
+	pushPair(vm);
+
+	pushInt(vm, 3);
+	pushInt(vm, 4);
+	pushPair(vm);
+
+	pushPair(vm);
+
+	gc(vm);
+	assert(vm->numObjects == 7, "Should have reached objects.");
+	freeVM(vm);
+}
+
+void test4() {
+	printf("Test 4: Handle cycles.\n");
+	VM* vm = newVM();
+	
+	pushInt(vm, 1);
+	pushInt(vm, 2);
+	Object* a = pushPair(vm);
+	printf("\tPushed a: "); objectPrint(a); printf("\n");
+
+	pushInt(vm, 3);
+	pushInt(vm, 4);
+	Object* b = pushPair(vm);
+	printf("\tPushed b: "); objectPrint(b); printf("\n");
+
+	// Set up a cycle, also making OBJ_INT(2) and OBJ_INT(4) unreachable and
+	// collectible.
+	printf("\tSetting up cyclical references between a's and b's tails.\n");
+	a->tail = b;
+	b->tail = a;
+
+	gc(vm);
+	assert(vm->numObjects == 4, "Should have collected objects.");
+	freeVM(vm);
+}
+
+void perfTest() {
+	printf("Starting performance test.\n");
+	VM* vm = newVM();
+
+	for (int i = 0; i < 10000; i++) {
+		for (int j = 0; j < 20; j++) {
+			pushInt(vm, i);
+		}
+
+		for (int k = 0; k < 20; k++) {
+			pop(vm);
+		}
+	}
+	
+	freeVM(vm);
+}
+
 int main(int argc, const char* argv[]) {
+	test1();
+	test2();
+	test3();
+	test4();
+	perfTest();
+
 	return 0;
 }
